16e639e20a342d2f78145bc5de78c08e6bfa7128
Choiyoungmi <choiyoungmi2252@gmail.com>
2022-12-05 18:03:28 +0900

    Ascending

---
+++ Ascending_03_05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ascending_03_05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] num = new int[10];
+		int[] p = new int[10];
+		int j=1;
+		
+		System.out.print("정수 10개 입력 >>");
+		
+		for(int i =0;i<10;i++) 
+			num[i] = sc.nextInt();
+		
+		for(int i =1;i<num.length;i++) {
+			if(num[i]>)
+		}
+		
+		for(int i =0;i<10;i++) 
+			System.out.println(p[i]);
+	}
+
+}
